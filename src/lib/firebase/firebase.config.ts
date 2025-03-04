49844c592fe0ed359ec11458050f54583a12d02e
WayanFWP <piwayan1@gmail.com>
2025-03-04 17:13:17 +0700

    add key

--- src/lib/firebase/firebase.config.ts
+++ src/lib/firebase/firebase.config.ts
@@ -1,21 +1,23 @@
 // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+// Import the functions you need from the SDKs you need
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID,
-    measurementId: process.env.FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyDOZSiMbdp164JxhEBOT0cp78lCYD6hlvo",
+  authDomain: "test-web-dbe21.firebaseapp.com",
+  projectId: "test-web-dbe21",
+  storageBucket: "test-web-dbe21.firebasestorage.app",
+  messagingSenderId: "34105707761",
+  appId: "1:34105707761:web:d75d7a198f5c41ed9f2d50"
 };
 
 // Initialize Firebase
-export const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { auth, app };
